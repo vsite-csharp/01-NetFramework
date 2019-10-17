2f42b4b95ff66d6ac5dfbc53ac23113585c1a1d2
don <don@DESKTOP-QROSDFJ>
2019-10-17 21:25:01 +0200

    -remove all TODOs

--- ClsCompliant2/TimeProvider.cs
+++ ClsCompliant2/TimeProvider.cs
@@ -1,9 +1,6 @@
 ﻿using System;
 
-// TODO:030 Napraviti build projekta ProvideTime i pogledati ima li pogrešaka ili upozorenja
-// TODO:031 Napraviti build projekta VbNetForm i pogledati koju pogrešku javlja
-// TODO:032 Otkomentirati donju naredbu i napraviti build projekta te provjeriti javlja li pogreške ili upozorenja
-//[assembly:CLSCompliant(true)]
+[assembly:CLSCompliant(true)]
 namespace ProvideTime
 {
     public class TimeProvider
@@ -18,8 +15,8 @@ namespace ProvideTime
             return DateTime.Now.ToString(); 
         }
 
-        // TODO:033 Promijeniti pravo pristupa donje metode iz public u private te napraviti build projekta VbNetForm, pokrenuti program i provjeriti funkcionalnost.
-        public static string GetDatetime()
+        
+        static string GetDatetime()
         {
             return DateTime.UtcNow.ToLongDateString();
         }
--- Hello World/Program.cs
+++ Hello World/Program.cs
@@ -2,11 +2,7 @@
 
 namespace Vsite.CSharp.NetFramework
 {
-    // TODO:000 Pogledati strukturu projekta: Properties, References...
-    // TODO:001 Kliknuti na projekt desnom tipkom miša i iz kontekstnog izbornika izabrati "Open Folder in File Explorer". Pogledati strukturu kazala.
-    // TODO:002 Napraviti build programa i pogledati koja kazala i datoteke je Visual Studio kreirao.
-    // TODO:003 Pokrenuti program i pogledati ispis.
-    // TODO:004 Pomoću programa ILDasm (obično unutar "C:\Program Files (x86)\Microsoft SDKs\Windows\vNN.N\bin\NETFX N.N.N Tools") pogledati sadržaj generirane izvedbene datoteke.
+    
     class Program
     {
         static void Main(string[] args)
