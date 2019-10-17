5e5b0136d7ff4d6dccd1332d3c87d2beda19c23d
Filip Velebit Samodol <fisamodo@vsite.hr>
2019-10-17 21:27:27 +0200

    Remove TODO

--- Pozivi metoda/Program.cs
+++ Pozivi metoda/Program.cs
@@ -13,13 +13,11 @@ namespace Vsite.CSharp.NetFrameWork
                 ++sum;
         }
 
-        // TODO:020 Pokrenuti program nekoliko puta i usporediti vremena ispisa
         static void Main(string[] args)
         {
             // štoperica
             Stopwatch stopWatch = new Stopwatch();
 
-            // TODO:021 Otkomentirati donjih šest naredbi i uporediti rezultate s prijašnjima
             stopWatch.Restart();
             stopWatch.Stop();
             Console.WriteLine($"Prvo pokretanje/zaustavljanje štoperice: {stopWatch.Elapsed.Ticks}");
