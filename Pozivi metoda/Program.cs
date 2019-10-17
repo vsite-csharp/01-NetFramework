ed6efc84be824a6c0e77e39af7230d4754745ebb
BanovJo <banov.josipa@gmail.com>
2019-10-17 21:25:42 +0200

    Remove TODOs.

--- Pozivi metoda/Program.cs
+++ Pozivi metoda/Program.cs
@@ -13,22 +13,20 @@ namespace Vsite.CSharp.NetFrameWork
                 ++sum;
         }
 
-        // TODO:020 Pokrenuti program nekoliko puta i usporediti vremena ispisa
         static void Main(string[] args)
         {
             // štoperica
             Stopwatch stopWatch = new Stopwatch();
 
-            // TODO:021 Otkomentirati donjih šest naredbi i uporediti rezultate s prijašnjima
-            //stopWatch.Restart();
-            //stopWatch.Stop();
-            //Console.WriteLine($"Prvo pokretanje/zaustavljanje štoperice: {stopWatch.Elapsed.Ticks}");
+            stopWatch.Restart();
+            stopWatch.Stop();
+            Console.WriteLine($"Prvo pokretanje/zaustavljanje štoperice: {stopWatch.Elapsed.Ticks}");
 
-            //stopWatch.Restart();
-            //stopWatch.Stop();
-            //Console.WriteLine($"Drugo pokretanje/zaustavljanje štoperice: {stopWatch.Elapsed.Ticks}");
+            stopWatch.Restart();
+            stopWatch.Stop();
+            Console.WriteLine($"Drugo pokretanje/zaustavljanje štoperice: {stopWatch.Elapsed.Ticks}");
 
-            // prvi poziv metode
+            //prvi poziv metode
             stopWatch.Restart();
             NekaMetoda();
             stopWatch.Stop();
