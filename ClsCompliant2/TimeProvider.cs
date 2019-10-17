e80873bedd57523fbfa25a61af284163ede36adb
Ivan Batinić <ivek81cro@gmail.com>
2019-10-17 21:24:40 +0200

    Finished TODO list

--- ClsCompliant2/TimeProvider.cs
+++ ClsCompliant2/TimeProvider.cs
@@ -1,9 +1,7 @@
 ﻿using System;
 
-// TODO:030 Napraviti build projekta ProvideTime i pogledati ima li pogrešaka ili upozorenja
-// TODO:031 Napraviti build projekta VbNetForm i pogledati koju pogrešku javlja
-// TODO:032 Otkomentirati donju naredbu i napraviti build projekta te provjeriti javlja li pogreške ili upozorenja
-//[assembly:CLSCompliant(true)]
+
+[assembly:CLSCompliant(true)]
 namespace ProvideTime
 {
     public class TimeProvider
@@ -18,8 +16,7 @@ namespace ProvideTime
             return DateTime.Now.ToString(); 
         }
 
-        // TODO:033 Promijeniti pravo pristupa donje metode iz public u private te napraviti build projekta VbNetForm, pokrenuti program i provjeriti funkcionalnost.
-        public static string GetDatetime()
+        private static string GetDatetime()
         {
             return DateTime.UtcNow.ToLongDateString();
         }
