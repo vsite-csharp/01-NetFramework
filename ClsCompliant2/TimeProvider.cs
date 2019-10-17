3439f74f6a3206630015cd494440699eeb266aa2
Domagoj Krmpotić <domagojkrmpoti3@outlook.com>
2019-10-17 21:25:10 +0200

    2. predavanja dkrmpotic

--- ClsCompliant2/TimeProvider.cs
+++ ClsCompliant2/TimeProvider.cs
@@ -1,9 +1,8 @@
 ﻿using System;
 
-// TODO:030 Napraviti build projekta ProvideTime i pogledati ima li pogrešaka ili upozorenja
-// TODO:031 Napraviti build projekta VbNetForm i pogledati koju pogrešku javlja
-// TODO:032 Otkomentirati donju naredbu i napraviti build projekta te provjeriti javlja li pogreške ili upozorenja
-//[assembly:CLSCompliant(true)]
+
+
+[assembly:CLSCompliant(true)]
 namespace ProvideTime
 {
     public class TimeProvider
@@ -19,7 +18,7 @@ namespace ProvideTime
         }
 
         // TODO:033 Promijeniti pravo pristupa donje metode iz public u private te napraviti build projekta VbNetForm, pokrenuti program i provjeriti funkcionalnost.
-        public static string GetDatetime()
+        static string GetDatetime()
         {
             return DateTime.UtcNow.ToLongDateString();
         }
