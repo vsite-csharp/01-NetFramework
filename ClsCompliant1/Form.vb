42235c40600c23de176c690238b91601a81cadd6
Mario Bilic <mario.bilic4@gmail.com>
2019-10-17 21:25:13 +0200

    Remove TODOs

--- ClsCompliant1/Form.vb
+++ ClsCompliant1/Form.vb
@@ -3,4 +3,8 @@
     Private Sub ButtonGetTime_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles ButtonGetTime.Click
         TextBoxTime.Text = ProvideTime.TimeProvider.GetDateTime()
     End Sub
+
+    Private Sub Form_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
 End Class
