d73d6debe204fb7962f8ffa4f71e1d39b731ab51
ivana andelic <54909467+ivana474@users.noreply.github.com>
2020-10-08 21:14:16 +0200

    implement file exit

--- MyWord/MainForm.Designer.cs
+++ MyWord/MainForm.Designer.cs
@@ -92,8 +92,9 @@
             // exitToolStripMenuItem
             // 
             this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
-            this.exitToolStripMenuItem.Size = new System.Drawing.Size(112, 22);
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
             this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
             // 
             // editToolStripMenuItem
             // 
--- MyWord/MainForm.cs
+++ MyWord/MainForm.cs
@@ -16,5 +16,10 @@ namespace MyWord
         {
             InitializeComponent();
         }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
     }
 }
