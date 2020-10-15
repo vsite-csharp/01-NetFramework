96766bd46070969bcc8f06b2a1918b5f09a1cd1b
tirupcic <tirupcic@vsite.hr>
2020-10-15 20:56:55 +0200

    Finish Hello world

--- Hello World/Program.cs
+++ Hello World/Program.cs
@@ -2,7 +2,6 @@
 
 namespace Vsite.CSharp.NetFramework
 {
-    // TODO:004 Pomoću programa ILDasm (obično unutar "C:\Program Files (x86)\Microsoft SDKs\Windows\vNN.N\bin\NETFX N.N.N Tools") pogledati sadržaj generirane izvedbene datoteke.
     class Program
     {
         //
