9d5be410141396b31fc57203abfa69fc00b5d20a
tirupcic <tirupcic@vsite.hr>
2020-10-15 20:41:54 +0200

    commit

--- Hello World/Program.cs
+++ Hello World/Program.cs
@@ -9,6 +9,7 @@ namespace Vsite.CSharp.NetFramework
     // TODO:004 Pomoću programa ILDasm (obično unutar "C:\Program Files (x86)\Microsoft SDKs\Windows\vNN.N\bin\NETFX N.N.N Tools") pogledati sadržaj generirane izvedbene datoteke.
     class Program
     {
+        //
         static void Main(string[] args)
         {
             int a = 14;
