9a137118b6469d169f830f1de8c56ed8c3ca61c0
Valentina Behin <72219767+ValentinaBehin@users.noreply.github.com>
2020-10-15 21:07:44 +0200

    finish c#-vbnet

---
+++ Hello World/Program1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Vsite.CSharp.NetFramework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 14;
+            double d = 3.14;
+            Console.WriteLine(a);
+            Console.WriteLine(d);
+            Console.WriteLine("Zdravo");
+            Console.ReadKey();
+        }
+    }
+}
