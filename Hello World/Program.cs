b3c8edbbf7306ea4593cd012dfa1ecad98dfb703
tirupcic <tirupcic@vsite.hr>
2020-10-15 20:48:52 +0200

    todo 003 delete

--- Hello World/Program.cs
+++ Hello World/Program.cs
@@ -2,8 +2,6 @@
 
 namespace Vsite.CSharp.NetFramework
 {
-    // TODO:002 Napraviti build programa i pogledati koja kazala i datoteke je Visual Studio kreirao.
-    // TODO:003 Pokrenuti program i pogledati ispis.
     // TODO:004 Pomoću programa ILDasm (obično unutar "C:\Program Files (x86)\Microsoft SDKs\Windows\vNN.N\bin\NETFX N.N.N Tools") pogledati sadržaj generirane izvedbene datoteke.
     class Program
     {
